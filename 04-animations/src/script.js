5b31baf412633a4328facacb47510118869695a2
wael-fadlallah <wael.fudlallah@gmail.com>
2022-12-03 17:34:09 +0400

    animations

---
+++ 04-animations/src/script.js
@@ -0,0 +1,69 @@
+import './style.css'
+import * as THREE from 'three'
+import gsap from 'gsap';
+
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+let time = Date.now();
+const clock = new THREE.Clock();
+
+// Animate with GSAP 
+// gsap.to(mesh.position, {
+//     duration: 1,
+//     delay: 1,
+//     x: 2,
+// });
+// gsap.to(mesh.position, {
+//     duration: 1,
+//     delay: 2,
+//     x: 0,
+// });
+
+// Animations
+const tick = () => {
+    // // Time
+    const current_time = Date.now();
+    const deltaTime = current_time - time;
+    time = current_time;
+
+    const elapsedTime = clock.getElapsedTime();
+
+
+    // Update object
+    mesh.position.y =  Math.sin(elapsedTime);
+    mesh.position.x =  Math.cos(elapsedTime);
+
+    // Render
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+};
+
+tick();
